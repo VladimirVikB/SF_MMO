2cdb1f62b9c24ddacbc1e1a3c561e4afa2386acf
Vladimir <120676027+VladimirVikB@users.noreply.github.com>
2023-08-08 21:30:37 +0300

    Initial commit

---
+++ MMO_board/MMO_board/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for MMO_board project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MMO_board.settings')
+
+application = get_wsgi_application()
